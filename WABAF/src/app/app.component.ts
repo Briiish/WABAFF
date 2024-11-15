7f57d66b82ef3628b9d4e33aeb3fa209d5d03d69
Briggitte HS <u201922070@upc.edu.pe>
2024-11-14 21:31:48 -0500

    User_Rol_Security
    Clases Users, Rol
    Seguridad front
    Sin querys

--- WABAF/src/app/app.component.ts
+++ WABAF/src/app/app.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { UsersComponent } from './components/users/users.component';
+import { LoginService } from './services/login.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    RouterLink,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    UsersComponent,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'WABAF';
+  rol: string = '';
+  constructor(private LoginService: LoginService) {}
+  cerrar() {
+    sessionStorage.clear();
+  }
+
+  verificar() {
+    this.rol = this.LoginService.showRole();
+    return this.LoginService.verificar();
+  }
+  isAdmin() {
+    return this.rol === 'ADMIN';
+  }
+
+  isClient() {
+    return this.rol === 'CLIENT';
+  }
 }
