9422bd04b0bed6055a8b06544aba41c241baf617
Briggitte HS <u201922070@upc.edu.pe>
2024-11-17 22:07:55 -0500

    Revert2
    Revertir por muchos errores

--- WABAF/src/app/models/Modalidad.ts
+++
@@ -1,4 +0,0 @@
-export class Modalidad{
-    idMo:number=0
-    descriMo:string=""
-}
--- WABAF/src/app/models/Profesor.ts
+++
@@ -1,9 +0,0 @@
-export class Profesor{
-    idProfe:number=0
-    codProfe:string=""
-    nombreProfe:string=""
-    apellidoProfe:string=""
-    cursoProfe:string=""
-    diaProfe:string=""
-    turnoProfe:string=""
-}
